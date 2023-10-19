28b51f617b1519e71c05560138a13285895e90dc
Joe Siewert <jsiewert@cloudbees.com>
2023-10-18 19:45:59 -0500

    Disable

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -39,6 +39,6 @@ Rails.application.configure do
 
   # Use native SQLite integers as booleans in Rails 5.2+
   if ActiveRecord.gem_version >= Gem::Version.new("5.2")
-    config.active_record.sqlite3.represent_boolean_as_integer = true
+#    config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
